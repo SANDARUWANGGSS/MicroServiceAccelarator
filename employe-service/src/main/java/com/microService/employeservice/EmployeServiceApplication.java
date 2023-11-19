2ddabe4c8f00d0867dab6370fecaab3d3d6838d3
Sandaruwan Gamage <62364412+SANDARUWANGGSS@users.noreply.github.com>
2023-11-19 11:09:35 +0530

    Pushing the employe service

---
+++ employe-service/src/main/java/com/microService/employeservice/EmployeServiceApplication.java
@@ -0,0 +1,13 @@
+package com.microService.employeservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeServiceApplication.class, args);
+	}
+
+}
