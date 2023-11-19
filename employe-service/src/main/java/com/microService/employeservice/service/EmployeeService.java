524b42847cf75172f55f9a06ae2bd3b75caf85d8
Sandaruwan Gamage <62364412+SANDARUWANGGSS@users.noreply.github.com>
2023-11-19 21:15:24 +0530

    updated service

--- employe-service/src/main/java/com/microService/employeservice/service/EmployeeService.java
+++ employe-service/src/main/java/com/microService/employeservice/service/EmployeeService.java
@@ -4,4 +4,6 @@ import com.microService.employeservice.dto.EmployeeDto;
 
 public interface EmployeeService {
     EmployeeDto saveEmployee(EmployeeDto employeeDto);
+
+    EmployeeDto getEmployeeById(Long id);
 }
