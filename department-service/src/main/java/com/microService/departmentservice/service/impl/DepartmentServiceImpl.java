1e8c7e517e56ca5febcd88074448991044d735c6
Sandaruwan Gamage <62364412+SANDARUWANGGSS@users.noreply.github.com>
2023-11-19 11:55:08 +0530

    Added department service impl

---
+++ department-service/src/main/java/com/microService/departmentservice/service/impl/DepartmentServiceImpl.java
@@ -0,0 +1,35 @@
+package com.microService.departmentservice.service.impl;
+
+import com.microService.departmentservice.dto.DepartmentDto;
+import com.microService.departmentservice.entity.Department;
+import com.microService.departmentservice.repository.DepartmentRepository;
+import com.microService.departmentservice.service.DepartmentService;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class DepartmentServiceImpl implements DepartmentService {
+
+    private DepartmentRepository departmentRepository;
+
+    @Override
+    public DepartmentDto saveDepartment(DepartmentDto departmentDto) {
+        Department department = new Department(
+                departmentDto.getId(),
+                departmentDto.getDepartmentName(),
+                departmentDto.getDepartmentDescription(),
+                departmentDto.getDepartmentCode()
+        );
+
+        Department savedDepartment =departmentRepository.save(department);
+
+        DepartmentDto savedDepartmentDto = new DepartmentDto(
+                savedDepartment.getId(),
+                savedDepartment.getDepartmentName(),
+                savedDepartment.getDepartmentDescription(),
+                savedDepartment.getDepartmentCode()
+        );
+        return savedDepartmentDto;
+    }
+}
