62c6f6a2d595fe68b4ec392840dc05c4c78d797f
Sandaruwan Gamage <62364412+SANDARUWANGGSS@users.noreply.github.com>
2023-11-19 12:40:05 +0530

    Added repos

--- department-service/src/main/java/com/microService/departmentservice/service/DepartmentService.java
+++ department-service/src/main/java/com/microService/departmentservice/service/DepartmentService.java
@@ -4,4 +4,5 @@ import com.microService.departmentservice.dto.DepartmentDto;
 
 public interface DepartmentService {
     DepartmentDto saveDepartment(DepartmentDto departmentDto);
+    DepartmentDto getDepartmentByCode(String code);
 }
