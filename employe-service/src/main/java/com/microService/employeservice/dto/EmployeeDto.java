032381c07d8ea7800e72612b6f538fd6884b01bb
Sandaruwan Gamage <62364412+SANDARUWANGGSS@users.noreply.github.com>
2023-11-19 20:20:14 +0530

    added dto

---
+++ employe-service/src/main/java/com/microService/employeservice/dto/EmployeeDto.java
@@ -0,0 +1,19 @@
+package com.microService.employeservice.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Column;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class EmployeeDto {
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private  String email;
+}
