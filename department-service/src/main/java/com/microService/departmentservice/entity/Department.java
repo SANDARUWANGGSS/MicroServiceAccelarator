c33614c4bc6d0d38c82c7d1b5938a73b209567e2
Sandaruwan Gamage <62364412+SANDARUWANGGSS@users.noreply.github.com>
2023-11-19 11:23:50 +0530

    departments

--- department-service/src/main/java/com/microService/departmentservice/entity/Department.java
+++ department-service/src/main/java/com/microService/departmentservice/entity/Department.java
@@ -1,12 +1,14 @@
 package com.microService.departmentservice.entity;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @Entity
 @Table(name = "department")
 public class Department {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    private String departmentName;
+    private String departmentDescription;
+    private String departmentCode;
 }
