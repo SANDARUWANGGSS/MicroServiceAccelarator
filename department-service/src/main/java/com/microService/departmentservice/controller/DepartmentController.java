99cd091d3ab71c6cddb71a7764403b1dc5002ca7
Sandaruwan Gamage <62364412+SANDARUWANGGSS@users.noreply.github.com>
2023-11-19 12:38:28 +0530

    Added department controller

--- department-service/src/main/java/com/microService/departmentservice/controller/DepartmentController.java
+++ department-service/src/main/java/com/microService/departmentservice/controller/DepartmentController.java
@@ -5,10 +5,7 @@ import com.microService.departmentservice.service.DepartmentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("api/departments")
@@ -21,5 +18,12 @@ public class DepartmentController {
     {
         DepartmentDto saveDepartment = departmentService.saveDepartment(departmentDto);
         return new ResponseEntity<>(saveDepartment, HttpStatus.CREATED);
+    }
+
+    @GetMapping("{department-code}")
+    public ResponseEntity<DepartmentDto> getDepartment(@PathVariable(value = "department-code") String departmentCode)
+    {
+        DepartmentDto departmentDto = departmentService.getDepartmentByCode(departmentCode);
+        return new ResponseEntity<>(departmentDto,HttpStatus.OK);
     }
 }
