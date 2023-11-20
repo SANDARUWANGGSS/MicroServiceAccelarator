295c6b0865c30713a126a76d56ef868dcff45ea6
Sandaruwan Gamage <62364412+SANDARUWANGGSS@users.noreply.github.com>
2023-11-20 10:06:52 +0530

    Changed employee service application

--- employe-service/src/main/java/com/microService/employeservice/EmployeServiceApplication.java
+++ employe-service/src/main/java/com/microService/employeservice/EmployeServiceApplication.java
@@ -2,12 +2,20 @@ package com.microService.employeservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class EmployeServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EmployeServiceApplication.class, args);
+	}
+
+	@Bean
+	public RestTemplate restTemplate()
+	{
+		return new RestTemplate();
 	}
 
 }
