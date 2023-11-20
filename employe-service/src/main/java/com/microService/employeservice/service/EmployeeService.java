0c2729fcc5a19399ec09e4715d7c2e24579ae5fc
Sandaruwan Gamage <62364412+SANDARUWANGGSS@users.noreply.github.com>
2023-11-20 10:11:37 +0530

    Changed employee service

--- employe-service/src/main/java/com/microService/employeservice/service/EmployeeService.java
+++ employe-service/src/main/java/com/microService/employeservice/service/EmployeeService.java
@@ -1,9 +1,10 @@
 package com.microService.employeservice.service;
 
+import com.microService.employeservice.dto.ApiResponseDto;
 import com.microService.employeservice.dto.EmployeeDto;
 
 public interface EmployeeService {
     EmployeeDto saveEmployee(EmployeeDto employeeDto);
 
-    EmployeeDto getEmployeeById(Long id);
+    ApiResponseDto getEmployeeById(Long id);
 }
