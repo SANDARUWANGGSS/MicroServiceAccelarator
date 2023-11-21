cd5c33e906fb49e5b675c85b029a3fafd06a34a8
Sandaruwan Gamage <62364412+SANDARUWANGGSS@users.noreply.github.com>
2023-11-21 07:53:24 +0530

    Added API client

--- employe-service/src/main/java/com/microService/employeservice/EmployeServiceApplication.java
+++ employe-service/src/main/java/com/microService/employeservice/EmployeServiceApplication.java
@@ -2,11 +2,13 @@ package com.microService.employeservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.reactive.function.client.WebClient;
 
 @SpringBootApplication
+@EnableFeignClients
 public class EmployeServiceApplication {
 
 	public static void main(String[] args) {
@@ -19,10 +21,10 @@ public class EmployeServiceApplication {
 //		return new RestTemplate();
 //	}
 
-	@Bean
-	public WebClient webClient()
-	{
-		return WebClient.builder().build();
-	}
+//	@Bean
+//	public WebClient webClient()
+//	{
+//		return WebClient.builder().build();
+//	}
 
 }
---
+++ employe-service/src/main/java/com/microService/employeservice/service/APIClient.java
@@ -0,0 +1,16 @@
+package com.microService.employeservice.service;
+
+import com.microService.employeservice.dto.DepartmentDto;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity; 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@FeignClient(url = "http://localhost:8080",value = "DEPARTMENT-SERVICE")
+public interface APIClient {
+    @GetMapping("api/departments/{department-code}")
+    DepartmentDto getDepartment(@PathVariable(value = "department-code") String departmentCode);
+}
+
+
